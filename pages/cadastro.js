994531bb7468a07b99e49aef40be7f138da9ae06
GIOVANNA MOSCHIONI <giovanna.moschioni@senaisp.edu.br>
2026-03-19 11:32:20 -0300

    logica login e cadastro

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "../componentes/container";
 import Titulo from "../componentes/titulo";
 import Label from "../componentes/label";
@@ -6,57 +7,102 @@ import Botao from "../componentes/botao";
 import Background from "../componentes/background";
 import { Alert, Image, View, Text, TouchableOpacity } from "react-native";
 
+export default function Cadastro({ navigation }) {
 
-export default function Cadastro({navigation}){
-  
-  
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [CPF, setcpf] = useState("");
+  const [idade, setidade] = useState("");
+
+  function Cadastrar() {
+
+    if (user === "" || email === "" || pass === "" || CPF === "" || idade === "") {
+
+      Alert.alert("ERRO", "Preencha todos os campos!");
+      console.log("azedou");
+
+    } else { 
+
+      Alert.alert("Sucesso!", "Cadastro realizado com sucesso!");
+      console.log("Estou aqui");
+      navigation.navigate ("Login");
+      
+    }
+
+  }
 
   return (
     <Background>
       <Container>
-        <Titulo tit={"Cadastro"}/>
-        
-        <View style={{flexDirection:"row", alignContent:"flex-start"}}>
-          <Label label={"User.:"}/>     
-          <Input placeholder={"Digite seu user..."}/>
-        </View>     
-        
-        <View style={{flexDirection:"row", alignContent:"flex-end"}}>
-          <Label label={"Email.:"}/>     
-          <Input placeholder={"Digite seu email..."}/>
-        </View>
-        
-        <View style={{flexDirection:"row", alignContent:"flex-end"}}>
-          <Label label={"Senha.:"}/>     
-          <Input placeholder={"Digite sua senha..."}/>
+        <Titulo tit={"Cadastro"} />
+
+        <View style={{ flexDirection: "row" }}>
+          <Label label={"User.:"} />
+          <Input
+            placeholder={"Digite seu user..."}
+            value={user}
+            onChangeText={setUser}
+          />
         </View>
 
-        <View style={{flexDirection:"row", alignContent:"flex-end"}}>
-          <Label label={"CPF.:"}/>     
-          <Input placeholder={"Digite sua senha..."}/>
+        <View style={{ flexDirection: "row" }}>
+          <Label label={"Email.:"} />
+          <Input
+            placeholder={"Digite seu email..."}
+            value={email}
+            onChangeText={setEmail}
+          />
         </View>
 
-        <View style={{flexDirection:"row", alignContent:"flex-end"}}>
-          <Label label={"Idade.:"}/>     
-          <Input placeholder={"Digite sua senha..."}/>
+        <View style={{ flexDirection: "row" }}>
+          <Label label={"Senha.:"} />
+          <Input
+            placeholder={"Digite sua senha..."}
+            value={pass}
+            onChangeText={setPass}
+            secureTextEntry={true}
+          />
         </View>
-        
-        <Botao 
-         
-          txtBtn={"Aperte para logar!"}
+
+        <View style={{ flexDirection: "row" }}>
+          <Label label={"CPF.:"} />
+          <Input
+            placeholder={"Digite seu CPF..."}
+            value={CPF}
+            onChangeText={setcpf}
+          />
+        </View>
+
+        <View style={{ flexDirection: "row" }}>
+          <Label label={"Idade.:"} />
+          <Input
+            placeholder={"Digite sua idade..."}
+            value={idade}
+            onChangeText={setidade}
+            keyboardType="numeric"
+          />
+        </View>
+
+        <Botao
+          txtBtn={"Cadastrar"}
+          onPress={Cadastrar}
         />
 
-       
-        <TouchableOpacity >
-          <Text style={{marginTop:15, color:"black", textAlign:"center"}} onPress={()=>navigation.navigate("Login")}>
+        <TouchableOpacity>
+          <Text
+            style={{ marginTop: 15, color: "black", textAlign: "center" }}
+            onPress={() => navigation.navigate("Login")}
+          >
             Já Possui Conta? Clique Aqui!
           </Text>
         </TouchableOpacity>
-        
-        <Image 
-          source={require("../assets/logo.png")} 
-          style={{height:100,width:178,marginTop: 30,marginBottom: 20}}
+
+        <Image
+          source={require("../assets/logo.png")}
+          style={{ height: 100, width: 178, marginTop: 30, marginBottom: 20 }}
         />
+
       </Container>
     </Background>
   );
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "../componentes/container";
 import Titulo from "../componentes/titulo";
 import Label from "../componentes/label";
@@ -9,7 +10,31 @@ import { Alert, Image, View, Text, TouchableOpacity } from "react-native";
 
 export default function Login({navigation}){
   
-  
+ const [user, setUser] = useState("");
+const [email, setEmail] = useState("");
+const [pass, setPass] = useState("");
+   
+ const log = () => {   
+ 
+
+  if(user.trim === "gim" && email.trim === "gim@gmail.com" && pass.trim === "123"){
+    Alert.alert("Sucesso!", "Usuário Logado com Sucesso!");
+     navigation.navigate("Cadastro");
+
+  } else if(user.trim === " " || email.trim === " " || pass.trim === " "){
+    Alert.alert("ERRO", "Favor preencher todos os campos!");
+
+  } else {
+    Alert.alert("ERRO!", "Usuário não cadastrado!");
+  }
+}
+
+
+
+const nav = ()=>{
+  navigation.navigate("Cadastro");
+}
+
 
   return (
     <Background>
@@ -18,27 +43,27 @@ export default function Login({navigation}){
         
         <View style={{flexDirection:"row", alignContent:"flex-start"}}>
           <Label label={"User.:"}/>     
-          <Input placeholder={"Digite seu user..."}/>
+          <Input placeholder={"Digite seu user..."} value={user} onChangeText={setUser} />
         </View>     
         
-        <View style={{flexDirection:"row", alignContent:"flex-end"}}>
+         <View style={{flexDirection:"row", alignContent:"flex-end"}}>
           <Label label={"Email.:"}/>     
-          <Input placeholder={"Digite seu email..."}/>
+         <Input placeholder={"Digite seu email..."} 
+        value={email} 
+         onChangeText={setEmail}
+/>
         </View>
+       
         
         <View style={{flexDirection:"row", alignContent:"flex-end"}}>
           <Label label={"Senha.:"}/>     
-          <Input placeholder={"Digite sua senha..."}/>
+          <Input placeholder={"Digite sua senha..."} value={pass} onChangeText={setPass}/>
         </View>
         
-        <Botao 
-         
-          txtBtn={"Aperte para logar!"}
-        />
-
+       <Botao txtBtn={"Aperte para logar!"} onPress={log}/>
        
         <TouchableOpacity>
-          <Text style={{marginTop:15, color:"black", textAlign:"center"}} onPress={()=>navigation.navigate("Cadastro")}>
+          <Text style={{marginTop:15, color:"black", textAlign:"center"}} onPress={nav}>
             Não tem cadastro? Clique Aqui!
           </Text>
         </TouchableOpacity>
